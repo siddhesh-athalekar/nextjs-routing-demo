251fad0c2905d2f47bbc52563db58b1ed5014148
siddhesh <siddhesh85@gmail.com>
2025-04-09 22:23:49 +0530

    Added catch-all segments in docs folder

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+export default async function Docs({ params }: { params: Promise<{ slug: String[] }> }){
+  const { slug } = await params;
+  if(slug.length === 2) {
+    return <h1>Viewing docs for feature {slug[0]} and concept {slug[1]}</h1>
+  } else if (slug.length === 1){
+    return <h1>Viewing docs for feature {slug[0]}</h1>
+  }
+  return <h1>Docs Home Page</h1>
+}
