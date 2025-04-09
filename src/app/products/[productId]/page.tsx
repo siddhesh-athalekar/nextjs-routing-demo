1fd573db6475a8d79ff83402a867737b0f1c133b
siddhesh <siddhesh85@gmail.com>
2025-04-09 20:20:25 +0530

    Added dynamic route

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,4 @@
-export default function ProductDetails(){
-  return <h1>Product Details</h1>;
+export default async function ProductDetails({ params }: { params: Promise<{ productId: string }>;}){
+  const productId = (await params).productId;
+  return <h1>Details about product {productId}</h1>;
 };
