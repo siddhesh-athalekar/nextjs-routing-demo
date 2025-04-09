c4b83cfc080fc7b9c93f0ae4136a9dd83f311a2b
siddhesh <siddhesh85@gmail.com>
2025-04-09 22:41:53 +0530

    Used hook and use client directive to show review and product id inside not found page

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function NotFound(){
+  const pathName = usePathname();
+  const productId = pathName.split("/")[2];
+  const reviewId = pathName.split("/")[4];
+
   return (
     <div>
-      <h2>Review not found</h2>
+      <h2>Review {reviewId} not found for product {productId}</h2>
     </div>
   );
 };
