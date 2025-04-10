0e462fafd6dddf779dbb8e32780abfc1a08a899e
siddhesh <siddhesh85@gmail.com>
2025-04-10 11:29:52 +0530

    Added header and footer layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer style={{ backgroundColor: 'ghostWhite', padding: '1rem' }}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   )
 }
