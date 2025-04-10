b312f7053ddb426d48a551bed7748192b45feab5
siddhesh <siddhesh85@gmail.com>
2025-04-10 11:06:26 +0530

    Added a private folder which is not visible in routes

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,8 @@
+export default function LibPage() {
+  return (
+    <div>
+      <h2>Lib Page</h2>
+      <p>This is a page in the _lib directory. This is a private folder and not visible in routing.</p>
+    </div>
+  );
+};
