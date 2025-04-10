ba9bdb22adbcb4c8c108a3d51a0bdb99cfb4164e
siddhesh <siddhesh85@gmail.com>
2025-04-10 11:34:06 +0530

    Seperated header and footer layout as components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'ghostWhite', padding: '1rem' }}>
+      <p>Footer</p>
+    </footer>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
+      <p>Header</p>
+    </header>
+  );
+};
